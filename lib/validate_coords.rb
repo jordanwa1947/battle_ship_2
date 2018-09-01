6ae3bb2e26e340c5e479a2e237c185c0bf9f0397
Jordan Whitten <jordanwa1947@gmail.com>
2018-09-01 08:54:05 -0600

    validating the first coordinate and tests

--- lib/validate_coords.rb
+++ lib/validate_coords.rb
@@ -1,6 +1,22 @@
 class ValidateCoords
 
   def ship2(coordinates)
-    coordinates
-  end 
+    coords_array = coordinates.split('')
+    coord1(coords_array)
+  end
+
+  def coord1(coord_array)
+    coords_array = coordinates.split('')
+    if coords_array.length != 5 or coords_array[2] != ' '
+      'please type valid coordinates'
+    elsif !(coords_array[0] =~ /[A-D]/) or !(coords_array =~ /[1-4]/)
+      'please type valid coordinates'
+    else
+      coordinates
+    end
+  end
+
+  def ship2_coord2
+
+  end
 end
--- test/validate_coords.rb
+++ test/validate_coords.rb
@@ -12,4 +12,9 @@ class ValidateCoordsTest < Minitest::Test
     validate_coords = ValidateCoords.new
     assert_equal 'A1 A2', validate_coords.ship2('A1 A2')
   end
+
+  def test_that_validate_coords_can_validate_ship2_coords
+    validate_coords = ValidateCoords.new
+    assert_equal 'please type valid coordinates', validate_coords.ship2('jvowv')
+  end
 end
