a38778e6a07fea3d49ef92403a369b53b31eb736
Jordan Whitten <jordanwa1947@gmail.com>
2018-09-02 14:06:54 -0600

    new fire prompt

--- lib/game_play.rb
+++ lib/game_play.rb
@@ -7,6 +7,7 @@ validate_coords = ValidateCoords.new
 computer_ships = ComputerShips.new
 player_board = BoardSetup.new
 computer_board = BoardSetup.new
+player_fire_range = BoardSetup.new
 
 invalid = 'please type valid coordinates'
 overlap = 'ships may not overlap'
@@ -22,27 +23,38 @@ The grid has A1 at the top left and D4 at the bottom right.
 
 Enter the squares for the two-unit ship:'
 
-ship2_coords = validate_coords.ship2(gets.chomp)
+ship2_coords = validate_coords.ship2(gets.chomp.upcase)
 while ship2_coords == invalid
   puts invalid
-  ship2_coords = validate_coords.ship2(gets.chomp)
+  ship2_coords = validate_coords.ship2(gets.chomp.upcase)
 end
 
 ship2_coords.split(' ').each { |coord| player_board.update_board(coord, '#')}
 player_board.display_board
 
 puts 'Now enter the square for the three-unit ship:'
-ship3_coords = validate_coords.ship3(ship2_coords, gets.chomp)
+ship3_coords = validate_coords.ship3(ship2_coords, gets.chomp.upcase)
+
 while ship3_coords == invalid or ship3_coords == overlap
   if ship3_coords == invalid
     puts invalid
-    ship3_coords = validate_coords.ship3(ship2_coords, gets.chomp)
+    ship3_coords = validate_coords.ship3(ship2_coords, gets.chomp.upcase)
   else ship3_coords == overlap
     puts overlap
     puts 'please try again'
-    ship3_coords = validate_coords.ship3(ship2_coords, gets.chomp)
+    ship3_coords = validate_coords.ship3(ship2_coords, gets.chomp.upcase)
   end
 end
+
 coord_array = ship3_coords.split(' ')
 coord_array.each { |coord| player_board.update_board(coord, '#') }
 player_board.display_board
+
+puts 'Choose a coordinate to fire at'
+coord = gets.chomp.upcase
+valid_invalid = validate_coords.board_coords.include?(coord)
+while valid_invalid == false
+  puts "sorry, that's not a valid coordinate"
+  coord = gets.chomp.upcase
+  valid_invalid = validate_coords.board_coords.include?(coord)
+end
